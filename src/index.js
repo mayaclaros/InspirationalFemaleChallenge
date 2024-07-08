8fe4fab57fdc0baf507c2c3420541109cda37a71
mayaclaros <123900092+mayaclaros@users.noreply.github.com>
2024-07-08 19:48:07 -0400

    Added timestamp

--- src/index.js
+++ src/index.js
@@ -8,3 +8,6 @@ function changeAffirmation() {
   let h2Element = document.querySelector("#Affirmations1");
   h2Element.textContent = "Cátaleya";
 }
+let today = moment().format("[Today is] dddd, MMMM D, YYYY");
+let todayElement = document.querySelector("#current-date");
+todayElement.innerHTML = today;
