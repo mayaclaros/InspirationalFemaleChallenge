002bd240b53e0352ca238b31dcba43bfac91f241
mayaclaros <123900092+mayaclaros@users.noreply.github.com>
2024-07-08 23:58:18 -0400

    Added timestamp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 function askInspire() {
-  alert("Who is a female that inspires you?");
+  let inspirationFemale = prompt("Who is a female that inspires you?");
+
+  askInspire(inspirationFemale);
+  alert("inspirationalFemale");
 }
 
-setTimeout(askInspire, 15000);
+setTimeout(askInspire, 60000);
 
 function changeAffirmation() {
   let h2Element = document.querySelector("#Affirmations1");
   h2Element.textContent = "Cátaleya";
 }
-let today = moment().format("[Today is] dddd, MMMM D, YYYY");
+let today = moment().format("[Today is] dddd, MMMM D, YYYY h:mm a");
 let todayElement = document.querySelector("#current-date");
 todayElement.innerHTML = today;
